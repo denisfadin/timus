563c21179563923720356c8a57db10350837c71a
Фадин Д.Э <fadin@usd-fadin1.corp.tensor.ru>
2017-07-18 17:18:43 +0300

    codeforces B 001

--- codeforces/B/001/main.cpp
+++ codeforces/B/001/main.cpp
@@ -15,11 +15,31 @@ int main()
 
       if( s.at( 0 ) == 'R' && s.at( 1 ) >= '1' && s.at( 1 ) <= '9' && s.find( 'C' ) != std::string::npos )
       {
-         std::cout << "RXCY" << std::endl;
+         // R1C1
+         //std::cout << "RXCY" << std::endl;
       }
       else
       {
-         std::cout << "A1" << std::endl;
+         // A1
+         size_t pos = 0;
+         for( ; s.at( pos ) >= 'A' && s.at( pos ) <= 'Z'; ++pos )
+            ;
+         std::cout << 'R' << ( s.c_str() + pos );
+
+         int32_t y = 0;
+         int32_t d = 1;
+
+         while( true )
+         {
+            --pos;
+            y += ( s.at( pos ) - 'A' + 1 ) * d;
+
+            if( pos == 0 )
+               break;
+
+            d *= 26;
+         }
+         std::cout << "C" << y << std::endl;
       }
    }
 
