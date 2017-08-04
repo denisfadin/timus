1182147b0a1f39f3b8c522ca89f891ea8e6f0e9e
Denis Fadin <denis.fadin@mail.ru>
2017-08-04 12:17:04 +0300

    codeforces A 837

--- codeforces/A/837/main.cpp
+++ codeforces/A/837/main.cpp
@@ -8,22 +8,17 @@ int main()
    std::cin >> N;
 
    std::string s;
-   s.resize( 1+N );
-   std::cin.read( &s[0], 1+N );
 
-   int16_t result = 0, word_result = 0;
-
-   for( char c : s )
+   int16_t result = 0;
+   while( std::cin >> s )
    {
-      if( c == ' ' )
-      {
-         result = std::max( result, word_result );
-         word_result = 0;
-      }
-      else if( c >= 'A' && c <= 'Z' )
-         ++word_result;
+      int16_t word_result = 0;
+      for( auto c : s )
+         if( std::isupper( c ) )
+            ++word_result;
+
+      result = std::max( result, word_result );
    }
-   result = std::max( result, word_result );
 
    std::cout << result << std::endl;
 
