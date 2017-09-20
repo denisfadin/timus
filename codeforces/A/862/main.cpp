a74a7eed4faedb0cf37630867a3e1c45a11cc8c0
Denis Fadin <denis.fadin@mail.ru>
2017-09-20 08:22:26 +0300

    codeforces A 862

--- codeforces/A/862/main.cpp
+++ codeforces/A/862/main.cpp
@@ -1,45 +1,26 @@
 #include <iostream>
 #include <stdint.h>
-#include <vector>
-#include <algorithm>
-
-int32_t const MAX = 101;
 
 int main()
 {
-   int16_t n;
-   std::cin >> n;
+   int16_t n, x;
+   std::cin >> n >> x;
 
-   int16_t x;
-   std::cin >> x;
+   int16_t cnt_less = 0;
+   bool x_exist = false;
 
-   std::vector< bool > buf( MAX, false );
    for( int16_t i = 0; i < n; ++i )
    {
-      int16_t num;
+      int32_t num;
       std::cin >> num;
-      buf[ num ] = true;
+
+      if( num < x )
+         ++cnt_less;
+      else if( num == x )
+         x_exist = true;
    }
 
-   auto min = std::find( std::begin( buf ), std::end( buf ), false );
-   int16_t pos = std::distance( std::begin( buf ), min );
-
-   int16_t result = 0;
-   if( x != pos )
-   {
-      if( x < pos )
-         result = 1;
-      else if( x > pos )
-      {
-         for( int16_t i = pos; i < x; ++i )
-            if( !buf[i] )
-               ++result;
-         if( buf[ x ] )
-            ++result;
-      }
-   }
-
-   std::cout << result << std::endl;
+   std::cout << ( x - cnt_less + ( x_exist ? 1 : 0 ) ) << std::endl;
 
    return 0;
 }
