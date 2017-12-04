28ec83cc55d401c62add460311c5ce2869ead0f2
fadin <fadin@tensor.ru>
2017-12-04 09:38:05 +0300

    timus 1014

--- timus/1014/main.cpp
+++ timus/1014/main.cpp
@@ -1,24 +1,29 @@
 #include <cstdint>
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <algorithm>
 
-std::vector< int32_t > factorize( int32_t n )
+int64_t getQforN( int32_t n )
 {
-   std::vector< int32_t > result;
+   if( n == 0 )
+      return 10;
+   else if( n == 1 )
+      return 1;
+   else
+   {
+      int64_t result = 0;
+      int64_t p = 1;
 
-   for( int32_t i = 2; i <= std::sqrt(n); ++i )
-      while( n % i == 0 )
+      for( int16_t d = 9; d > 1; --d )
       {
-         result.push_back( i );
-         n /= i;
+         while( n % d == 0 )
+         {
+            n /= d;
+            result += d * p;
+            p *= 10;
+         }
       }
 
-   if( n != 1 || result.size()==0 )
-      result.push_back( n );
-
-   return result;
+      return n == 1 ? result : -1;
+   }
 }
 
 int main()
@@ -26,26 +31,7 @@ int main()
    int32_t n;
    std::cin >> n;
 
-   auto factors = factorize( n );
-
-   while( true )
-   {
-      std::sort( std::begin( factors ), std::end( factors ) );
-
-      if( factors.size() < 2 )
-         break;
-
-      auto d = factors[0] * factors[1];
-      if( d > 9 )
-         break;
-
-      factors.erase( factors.begin(), factors.begin()+2 );
-      factors.emplace_back( std::move( d ) );
-   }
-
-   for( auto const& c : factors )
-      std::cout << c;
-   std::cout << std::endl;
+   std::cout << getQforN( n ) << std::endl;
 
    return 0;
 }
