be89fc3b7c5ec7257a59c0b7af19041cc9366faa
fadin <fadin@tensor.ru>
2020-07-24 18:11:45 +0300

    timus 1002

--- timus/1002/1002.cpp
+++ timus/1002/1002.cpp
@@ -1,103 +1,14 @@
 #include <iostream>
 #include <string>
-#include <vector>
+#include <cinttypes>
+#include <stdexcept>
 #include <unordered_map>
-#include <list>
-#include <algorithm>
+#include <unordered_set>
+#include <vector>
 
-// Переводит слово в номер по заданной таблице соответствия
-std::string Word2Number( std::string const& word );
-
-std::list< size_t > find_min( size_t pos, size_t end_pos, std::unordered_map< size_t, std::list< std::pair< size_t, size_t > > > const& map, std::vector< std::string > const& words )
+char Letter2Digit( char letter )
 {
-   std::list< size_t > result;
-
-   auto it = map.find( pos );
-   if( it != map.end() )
-   {
-      for( std::pair< size_t, size_t > const& p : it->second )
-      {
-         std::string const& word = words[p.first];
-         if( pos + p.second == end_pos )
-         {
-            result.clear();
-            result.push_back( p.first );
-            break;
-         }
-         else
-         {
-            auto res = find_min( pos + p.second, end_pos, map, words );
-            if( res.size() != 0 && ( result.size() == 0 || result.size()-1 > res.size() ) )
-            {
-               result.clear();
-               result.push_back( p.first );
-               result.splice( result.end(), res );
-            }
-         }
-      }
-   }
-   return result;
-}
-
-int main()
-{
-   std::string number;
-   while( true )
-   {
-      std::cin >> number;
-      if( number == "-1" )
-         break;
-
-      //std::cout << number << std::endl;
-
-      size_t cnt = 0;
-      std::cin >> cnt;
-
-      //std::cout << cnt << std::endl;
-
-      std::vector< std::string > words;
-      words.resize( cnt );
-
-      std::unordered_map< size_t, std::list< std::pair< size_t, size_t > > > map;
-
-      for( size_t i = 0; i < cnt; ++i )
-      {
-         std::string& word = words[i];
-         std::cin >> word;
-         std::string word_number = Word2Number( word );
-
-         //std::cout << word << " -> " << word_number << " (";
-         size_t pos = number.find( word_number );
-         while( pos != std::string::npos )
-         {
-            //std::cout << pos << ",";
-            std::list< std::pair< size_t, size_t > >& l = map[ pos ];
-            if( std::find_if( l.begin(), l.end(), [&word_number]( std::pair< size_t, size_t > const& p ) -> bool { return p.second == word_number.size(); }) == l.end() )
-            {
-               l.push_back( std::make_pair( i, word_number.size() ) );
-            }
-
-            pos = number.find( word_number, pos + word_number.size() );
-         }
-         //std::cout << ")" << std::endl;
-      }
-
-      std::list< size_t > result = find_min( 0, number.size(), map, words );
-      if( result.size() == 0 )
-         std::cout << "No solution." << std::endl;
-      else
-      {
-         for( size_t i : result )
-            std::cout << words[i] << " ";
-         std::cout << std::endl;
-      }
-   }
-   return 0;
-}
-
-char Char2Digit( char c )
-{
-   switch( c )
+   switch( letter )
    {
       case 'i':
       case 'j':
@@ -136,14 +47,104 @@ char Char2Digit( char c )
       case 'z':
          return '0';
    }
-   return '?';
+   throw std::invalid_argument( "Invalid argument of Letter2Digit" );
 }
 
 std::string Word2Number( std::string const& word )
 {
-   std::string number;
-   number.resize( word.size() );
-   for( size_t i = 0; i < word.size(); ++i )
-      number[i] = Char2Digit( word[i] );
-   return number;
+   std::string result;
+   result.reserve( word.size() );
+   for( char sym : word )
+      result.push_back( Letter2Digit( sym ) );
+   return result;
+}
+
+int main()
+{
+   while( true )
+   {
+      std::string number;
+      std::cin >> number;
+
+      if( number == "-1" )
+         break;
+
+      uint32_t words_count;
+      std::cin >> words_count;
+
+      struct Edge
+      {
+         size_t mTo;
+         std::string mWord;
+      };
+      std::unordered_map< size_t, std::vector< Edge > > edges;
+
+      for( uint32_t i = 0; i < words_count; ++i )
+      {
+         std::string word;
+         std::cin >> word;
+
+         std::string word_number = Word2Number( word );
+
+         size_t pos = 0;
+         while( true )
+         {
+            pos = number.find( word_number, pos );
+            if( pos == std::string::npos )
+               break;
+
+            std::vector< Edge >& e = edges[ pos ];
+            e.push_back( Edge{ .mTo = pos + word.size(), .mWord = word } );
+
+            ++pos;
+            if( pos >= number.size() )
+               break;
+         }
+      }
+
+      std::vector< size_t > to_process;
+      to_process.push_back( 0 );
+      std::unordered_set< size_t > processed;
+
+      std::unordered_map< size_t, std::vector< const char* > > points;
+      while( !to_process.empty() )
+      {
+         size_t p = to_process.back();
+         to_process.pop_back();
+         if( processed.find( p ) != processed.end() )
+            continue;
+         processed.insert( p );
+
+         std::vector< const char* > const& pos_words = points[ p ];
+
+         auto it_edges = edges.find( p );
+         if( it_edges == edges.end() )
+            continue;
+
+         for( auto const& edge : it_edges->second )
+         {
+            std::vector< const char* >& words = points[ edge.mTo ];
+            if( words.empty() || ( words.size() > pos_words.size() + 1 ) )
+            {
+               words = pos_words;
+               words.push_back( edge.mWord.c_str() );
+            }
+            to_process.push_back( edge.mTo );
+         }
+      }
+
+      auto it = points.find( number.size() );
+      if( it == points.end() )
+      {
+         std::cout << "No solution." << std::endl;
+      }
+      else
+      {
+         for( const char* word : it->second )
+            std::cout << word << " ";
+         std::cout << std::endl;
+      }
+   }
+
+   return 0;
 }
