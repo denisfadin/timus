455cd49e215c32819a5c62410e9bce03b39b9150
fadin <fadin@tensor.ru>
2020-09-17 13:24:30 +0300

    put things in order

--- codeforces/B/862/gen.py
+++
@@ -1,6 +0,0 @@
-f = open( './test', 'w' )
-n = 100000
-f.write( str(n)+'\n' )
-for x in range(1, n):
-   f.write( str( x ) + ' ' + str( x+1 ) + '\n' );
-f.close();
--- codeforces/C/681/gen.py
+++
@@ -1,6 +0,0 @@
-f = open( './test3', 'w' )
-n = 100000
-f.write( str(n)+'\n' )
-for x in range(n, 0, -1):
-   f.write( 'getMin ' + str( x ) + '\n' );
-f.close();
