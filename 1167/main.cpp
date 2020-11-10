8b188c2e22099f367e347d246687bb34dc0317fe
Denis Fadin <denis.fadin@gmail.com>
2020-11-10 12:11:41 +0300

    timus 1167

---
+++ 1167/main.cpp
@@ -0,0 +1,62 @@
+#include <cstdint>
+#include <cstdio>
+#include <cinttypes>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+   uint_fast16_t N, K;
+   std::scanf( "%" SCNuFAST16 "%" SCNuFAST16, &N, &K );
+
+   std::vector< bool > horses( N + 1 );
+   for( uint_fast16_t i = 1; i <= N; ++i )
+   {
+      uint_fast16_t h;
+      std::scanf( "%" SCNuFAST16, &h );
+      horses[ i ] = static_cast< bool >( h );
+   }
+
+   std::unordered_map< uint32_t, uint16_t > result;
+
+   {
+      auto const until = N - ( K - 1 );
+      uint_fast16_t b = 0, w = 0;
+      for( uint_fast16_t i = 1; i <= until; ++i )
+      {
+         if( horses[ i ] )
+            ++b;
+         else
+            ++w;
+         result[ ( 1 << 16 ) + i ] = b * w;
+      }
+
+      for( uint_fast16_t k = 2; k <= K; ++k )
+      {
+         auto const until = N - ( K - k );
+         for( uint_fast16_t i = k; i <= until; ++i )
+         {
+            uint_fast16_t b = 0, w = 0;
+            for( uint_fast16_t j = i; j <= until; ++j )
+            {
+               if( horses[ j ] )
+                   ++b;
+                else
+                   ++w;
+
+               uint16_t unhappiness = b * w + result[ ( (k - 1 ) << 16 ) + ( i - 1 ) ];
+               auto key = ( k << 16 ) + j;
+               auto it = result.find( key );
+               if( it == result.end() )
+                  result[ key ] = unhappiness;
+               else
+                  result[ key ] = std::min( result[ key ], unhappiness );
+            }
+         }
+      }
+   }
+
+   std::printf( "%" PRIu32 "\n", result[ ( K << 16 ) + N ] );
+
+}
