9cb924184a85d207869e94ec75ed849d3e75fdaa
Denis Fadin <denis.fadin@gmail.com>
2020-11-13 19:50:16 +0300

    timus 1915

--- 1915/main.cpp
+++ 1915/main.cpp
@@ -5,16 +5,16 @@
 
 int main()
 {
-   uint32_t N;
-   std::scanf( "%" SCNu32, &N );
+   uint_fast32_t N;
+   std::scanf( "%" SCNuFAST32, &N );
 
-   std::vector< int32_t > stack;
+   std::vector< int_fast32_t > stack;
    stack.reserve( N );
 
-   for( uint32_t i = 0; i < N; ++i )
+   for( uint_fast32_t i = 0; i < N; ++i )
    {
-      int32_t command;
-      std::scanf( "%" SCNd32, &command );
+      int_fast32_t command;
+      std::scanf( "%" SCNdFAST32, &command );
 
       if( command > 0 )
          stack.push_back( std::move( command ) );
@@ -30,7 +30,7 @@ int main()
             auto& back = stack.back();
             if( back > 0 )
             {
-               std::printf( "%" PRId32 "\n", stack.back() );
+               std::printf( "%" PRIdFAST32 "\n", stack.back() );
                stack.pop_back();
                break;
             }
@@ -44,7 +44,7 @@ int main()
                auto val = stack[ pos ];
                if( val > 0 )
                {
-                  std::printf( "%" PRId32 "\n", val );
+                  std::printf( "%" PRIdFAST32 "\n", val );
                   break;
                }
 
