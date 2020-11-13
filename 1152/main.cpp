b0cb3c3817f1ce1954fa383d9a48b41320a08c13
Denis Fadin <denis.fadin@gmail.com>
2020-11-13 14:00:20 +0300

    timus 1152

---
+++ 1152/main.cpp
@@ -0,0 +1,88 @@
+#include <cstdint>
+#include <cstdio>
+#include <cinttypes>
+#include <vector>
+#include <limits>
+#include <array>
+#include <unordered_map>
+
+uint32_t Do( uint_least32_t map,
+             std::array< uint32_t, 32 > const& monsters,
+             std::vector< uint_least32_t > const& shots )
+{
+   if( map == 0 )
+      return 0;
+
+   static std::unordered_map< uint_least32_t, uint32_t > cache;
+   auto it = cache.find( map );
+   if( it != cache.end() )
+      return it->second;
+
+   uint32_t result = std::numeric_limits< uint32_t >::max();
+   for( auto const shot : shots )
+   {
+      if( ( map & shot ) == 0 )
+         continue;
+
+      uint32_t new_map = map & (~shot);
+
+      uint32_t res = 0;
+      uint_least32_t m = 1;
+      for( uint32_t i = 0; i < 32; ++i )
+      {
+         if( m & new_map )
+            res += monsters[ i ];
+         m <<= 1;
+      }
+      res += Do( new_map, monsters, shots );
+      if( res < result )
+         result = res;
+   }
+
+   cache[ map ] = result;
+
+   return result;
+}
+
+int main()
+{
+   uint32_t N;
+   std::scanf( "%" SCNu32, &N );
+
+   std::array< uint32_t, 32 > monsters;
+   monsters.fill( 0 );
+   for( uint32_t i = 0; i < N; ++i )
+      std::scanf( "%" SCNu32, &monsters[ i ] );
+
+   std::vector< uint_least32_t > shots;
+   {
+      uint_least32_t m = 7; // 111 в двоичном представлении
+      for( uint32_t i = 1; i < N - 1; ++i )
+      {
+         shots.push_back( m );
+         m <<= 1;
+      }
+
+      if( N > 3 )
+      {
+         m = 1 << ( N - 1 );
+         m |= 3;
+         shots.push_back( m );
+
+         m = 3 << ( N - 2 );
+         m |= 1;
+         shots.push_back( m );
+      }
+   }
+
+   uint_least32_t map = 7;
+   for( uint32_t i = 3; i < N; ++i )
+   {
+      map <<= 1;
+      map |= 1;
+   }
+
+   std::printf( "%" PRIu32 "\n", Do( map, monsters, shots ) );
+
+   return 0;
+}
