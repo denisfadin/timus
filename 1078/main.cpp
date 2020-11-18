0a6dc9ebc0dad0559028c3b6225df22a6040e720
Denis Fadin <denis.fadin@gmail.com>
2020-11-18 13:16:21 +0300

    timus 1078

--- 1078/main.cpp
+++ 1078/main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <utility>
 #include <algorithm>
+#include <limits>
 
 int main()
 {
@@ -13,17 +14,8 @@ int main()
    using SegmentT = std::pair< int16_t, int16_t >;
 
    std::vector< SegmentT > segments( N );
-   std::vector< uint16_t > segments_poses( N );
    for( uint16_t i = 0; i < N; ++i )
-   {
       std::scanf( "%" SCNd16 "%" SCNd16, &segments[ i ].first, &segments[ i ].second );
-      segments_poses[ i ] = i;
-   }
-
-   std::sort( segments_poses.begin(), segments_poses.end(), [ & ]( uint16_t a, uint16_t b )
-   {
-      return segments[ a ].second < segments[ b ].second;
-   });
 
    struct CacheData
    {
@@ -33,29 +25,28 @@ int main()
    };
 
    std::vector< CacheData > cache;
-
-   for( auto segment_pos : segments_poses )
+   cache.reserve( N );
+   for( uint16_t i = 0; i < N; ++i )
+      cache.push_back( CacheData{ .mSegmentPos = i, .mCount = 1, .mPrev = std::numeric_limits< uint16_t >::max() } );
+   std::sort( cache.begin(), cache.end(), [ & ]( CacheData const& a, CacheData const& b )
    {
-      auto const& segment = segments[ segment_pos ];
+      return segments[ a.mSegmentPos ].second < segments[ b.mSegmentPos ].second;
+   });
 
-      CacheData data;
-      data.mSegmentPos = segment_pos;
-      data.mCount = 1;
-      data.mPrev = cache.size();
-
-      for( uint16_t i = 0; i < cache.size(); ++i )
+   for( uint16_t i = 0; i < cache.size(); ++i )
+   {
+      auto const& segment = segments[ cache[ i ].mSegmentPos ];
+      for( uint16_t j = 0; j < i; ++j )
       {
-         auto const& tmp_segment = segments[ cache[ i ].mSegmentPos ];
+         auto const& tmp_segment = segments[ cache[ j ].mSegmentPos ];
          if( tmp_segment.first <= segment.first || tmp_segment.second >= segment.second )
             continue;
-         if( data.mCount < cache[ i ].mCount + 1 )
+         if( cache[ i ].mCount < cache[ j ].mCount + 1 )
          {
-            data.mCount = cache[ i ].mCount + 1;
-            data.mPrev = i;
+            cache[ i ].mCount = cache[ j ].mCount + 1;
+            cache[ i ].mPrev = j;
          }
       }
-
-      cache.push_back( std::move( data ) );
    }
 
    uint16_t result_pos = 0;
@@ -64,11 +55,9 @@ int main()
          result_pos = i;
 
    std::vector< uint16_t > result_segments;
-   while( true )
+   while( result_pos != std::numeric_limits< uint16_t >::max() )
    {
       result_segments.push_back( cache[ result_pos ].mSegmentPos );
-      if( result_pos == cache[ result_pos ].mPrev )
-         break;
       result_pos = cache[ result_pos ].mPrev;
    }
 
