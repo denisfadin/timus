a4b5db4f50f3ede1efdec99bd9bd68fb21490ab9
Denis Fadin <denis.fadin@gmail.com>
2020-12-02 11:04:07 +0300

    timus 1881

--- 1881/main.cpp
+++ 1881/main.cpp
@@ -1,20 +1,23 @@
 #include <cstdint>
-#include <iostream>
-#include <string>
+#include <cstdio>
+#include <cinttypes>
+#include <cstring>
+#include <array>
 
 int main()
 {
    uint16_t H, W, N;
-   std::cin >> H >> W >> N;
+   std::scanf( "%" SCNu16 "%" SCNu16 "%" SCNu16, &H, &W, &N );
 
    uint16_t result_lines_count = 0;
 
    uint16_t curr_line_size = 0;
-   std::string word;
+   std::array< char, 101 > word;
    for( uint16_t i = 0; i < N; ++i )
    {
-      std::cin >> word;
-      if( curr_line_size + ( curr_line_size ? 1 : 0 ) + word.size() > W )
+      std::scanf( "%s", word.data() );
+      auto word_size = std::strlen( word.data() );
+      if( curr_line_size + ( curr_line_size ? 1 : 0 ) + word_size > W )
       {
          ++result_lines_count;
          curr_line_size = 0;
@@ -22,12 +25,12 @@ int main()
       else if( curr_line_size )
          ++curr_line_size;
 
-      curr_line_size += word.size();
+      curr_line_size += word_size;
    }
    if( curr_line_size )
       ++result_lines_count;
 
-   std::cout << result_lines_count / H + (( result_lines_count % H ) ? 1 : 0) << std::endl;
+   std::printf( "%" PRIu16 "\n", result_lines_count / H + (( result_lines_count % H ) ? 1 : 0));
 
    return 0;
 }
