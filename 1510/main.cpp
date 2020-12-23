b2137112fa7156f13a49bb5e975604f0fe901a39
Denis Fadin <denis.fadin@mail.ru>
2020-12-23 23:48:40 +0300

    timus 1510

--- 1510/main.cpp
+++ 1510/main.cpp
@@ -1,30 +1,21 @@
 #include <cstdint>
 #include <cstdio>
 #include <cinttypes>
-#include <unordered_map>
+#include <vector>
+#include <algorithm>
 
 int main()
 {
    uint32_t N;
    std::scanf( "%" SCNu32, &N );
 
-   std::unordered_map< uint32_t, uint32_t > cache;
-   cache.reserve( N >> 1 );
-   uint32_t k;
+   std::vector< uint32_t > data( N );
    for( uint32_t i = 0; i < N; ++i )
-   {
-      std::scanf( "%" SCNu32, &k );
-      auto it = cache.find( k );
-      if( it == cache.end() )
-         it = cache.emplace( k, 0 ).first;
-      it->second += 1;
-   }
-   auto result = cache.begin();
-   for( auto it = cache.begin(); it != cache.end(); ++it )
-      if( result->second < it->second )
-         result = it;
+      std::scanf( "%" SCNu32, &data[ i ] );
 
-   std::printf( "%" PRIu32 "\n", result->first );
+   std::sort( data.begin(), data.end() );
+
+   std::printf( "%" PRIu32 "\n", data[ N >> 1 ] );
 
    return 0;
 }
