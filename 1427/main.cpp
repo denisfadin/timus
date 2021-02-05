90be63e1f946779aeaf48509e657d913dcfb921a
fadin <fadin@tensor.ru>
2021-02-05 21:33:53 +0300

    timus 1427

--- 1427/main.cpp
+++ 1427/main.cpp
@@ -12,12 +12,15 @@ int main()
    std::scanf( "%" SCNu32 "%" SCNu32 " ", &N, &M );
 
    std::vector< char > text( 100001, 0 );
+   std::vector< uint32_t > suitable_symbols_data( 100001, 0 );
    for( uint32_t i = 0; ; ++i )
    {
       char sym = std::getchar();
       if( sym == '\n' || sym == EOF )
          break;
       text[ i ] = sym;
+      if( std::isalpha( sym ) || sym == ' ' )
+         suitable_symbols_data[ i + 1 ] = suitable_symbols_data[ i ] + 1;
    }
 
    uint32_t text_size = std::strlen( text.data() );
@@ -28,39 +31,21 @@ int main()
    }
 
    std::vector< uint32_t > dp( text_size, 1 );
-
-   auto is_suitable_symbol = [ &text ]( uint32_t i )
-   {
-      return std::isalpha( text[ i ] ) || text[ i ] == ' ';
-   };
-
    for( uint32_t i = N; i < text_size; ++i )
    {
-      dp[ i ] = dp[ i - 1 ] + 1;
-      bool only_suitable_symbols = is_suitable_symbol( i );
-      for( uint32_t j = 1; j <= N; ++j )
+      dp[ i ] = dp[ i - N ] + 1;
+
+      uint32_t TO = std::min( suitable_symbols_data[ i + 1 ], M );
+      if( TO <= N )
+         continue;
+
+      if( TO > i )
       {
-         dp[ i ] = std::min( dp[ i ], dp[ i - j ] + 1 );
-         if( only_suitable_symbols && !is_suitable_symbol( i - j ) && j < N )
-            only_suitable_symbols = false;
+         dp[ i ] = 1;
+         continue;
       }
 
-      if( only_suitable_symbols )
-      {
-         for( uint32_t j = N + 1; j <= M; ++j )
-         {
-            if( !is_suitable_symbol( ( i + 1 ) - j ) )
-               break;
-
-            if( j > i )
-            {
-               dp[ i ] = 1;
-               break;
-            }
-
-            dp[ i ] = std::min( dp[ i ], dp[ i - j ] + 1 );
-         }
-      }
+      dp[ i ] = std::min( dp[ i ], dp[ i - TO ] + 1 );
    }
 
    std::printf( "%" PRIu32 "\n", dp[ text_size - 1 ] );
