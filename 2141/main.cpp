c27b7433cb06664c336bf76a161e757e35adbab4
fadin <fadin@tensor.ru>
2021-02-09 15:38:07 +0300

    timus 2141

--- 2141/main.cpp
+++ 2141/main.cpp
@@ -1,35 +1,31 @@
 #include <cstdint>
 #include <cstdio>
 #include <cinttypes>
-#include <vector>
-#include <algorithm>
 
 int main()
 {
    uint32_t N;
    std::scanf( "%" SCNu32, &N );
 
-   std::vector< int64_t > positive( N );
-   std::vector< int64_t > negative( N );
-
-   std::scanf( "%" SCNd64, &positive[ 0 ] );
-   negative[ 0 ] = 0;
+   int64_t positive;
+   std::scanf( "%" SCNd64, &positive );
+   int64_t negative = 0;
+   int64_t result = positive > negative ? positive : negative;
    for( uint32_t i = 1; i < N; ++i )
    {
-      std::scanf( "%" SCNd64, &positive[ i ] );
-      negative[ i ] = positive[ i - 1 ] - positive[ i ];
-      if( negative[ i - 1 ] > 0 )
-         positive[ i ] += negative[ i - 1 ];
-   }
+      int64_t p;
+      std::scanf( "%" SCNd64, &p );
+      int64_t n = positive - p;
+      if( negative > 0 )
+         p += negative;
 
-   int64_t result = std::max( positive[ 0 ], negative[ 0 ] );
-   for( uint32_t i = 1; i < N; ++i )
-   {
-      if( result < positive[ i ] )
-         result = positive[ i ];
+      if( result < p )
+         result = p;
+      if( result < n )
+         result = n;
 
-      if( result < negative[ i ] )
-         result = negative[ i ];
+      positive = p;
+      negative = n;
    }
 
    std::printf( "%" PRId64 "\n", result );
