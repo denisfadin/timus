021e9141e4c35f38a607d2f0d2ec504f825c84d3
fadin <fadin@tensor.ru>
2021-02-15 13:01:11 +0300

    timus 1513

--- 1513/main.py
+++ 1513/main.py
@@ -1,19 +1,20 @@
 import sys
+from collections import deque
 
 def main():
     N, K = map(int, input().split())
 
-    dp = [0]*(N+2)
-    dp[0] = 1
-    dp[1] = 1
+    dp = deque()
+    dp.append(1)
+    dp.append(1)
     for i in range(2, K+2):
-        dp[i] = dp[i-1] << 1
-    for i in range(K+2, N+1):
-        dp[i] = (dp[i-1] << 1) - dp[i-K-2]
+        dp.append(dp[-1] << 1)
+    for i in range(K+2, N+2):
+        dp.append((dp[-1] << 1)-dp.popleft())
 
     result = 0
-    for i in range(0, K+1):
-        result += dp[N-i]
+    for i in range(1, K+2):
+        result += dp[-1-i]
     print(result)
 
 if __name__ == '__main__':
