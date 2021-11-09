c7fd49f6987f5d8288baeb2dea2c490d816e9109
Denis Fadin <denis.fadin@gmail.com>
2021-11-09 19:27:33 +0300

    timus 1930

--- 1930/main.cpp
+++ 1930/main.cpp
@@ -2,7 +2,7 @@
 #include <cstdint>
 #include <cinttypes>
 #include <vector>
-#include <deque>
+#include <queue>
 #include <limits>
 
 int main()
@@ -11,22 +11,26 @@ int main()
    std::scanf( "%" SCNu32 " %" SCNu32, &N, &M );
 
    std::vector< bool > up_roads( (N+1)*(N+1), false );
-   std::vector< bool > down_roads( (N+1)*(N+1), false );
+
+   auto is_up_road = [ &up_roads, &N ]( uint32_t x, uint32_t y )
+   {
+      return up_roads[ x*(N+1)+y ];
+   };
 
    for( uint32_t i = 0; i < M; ++i )
    {
       uint32_t x, y;
       std::scanf( "%" SCNu32 " %" SCNu32, &x, &y );
       up_roads[ x*(N+1)+y ] = true;
-      down_roads[ y*(N+1)+x ] = true;
    }
 
    uint32_t A, B;
    std::scanf( "%" SCNu32 " %" SCNu32, &A, &B );
 
    std::vector< bool > checked( N+1, false );
-   std::deque< uint32_t > to_process;
-   to_process.push_back( A );
+   std::queue< uint32_t > to_process;
+   checked[ A ] = true;
+   to_process.push( A );
 
    std::vector< uint32_t > up( N+1, std::numeric_limits< uint32_t >::max() );
    up[A]=0;
@@ -36,26 +40,29 @@ int main()
    while( !to_process.empty() )
    {
       auto p = to_process.front();
-      to_process.pop_front();
-      if( checked[p] )
-         continue;
-      checked[p] = true;
+      to_process.pop();
 
       for( uint32_t i = 1; i <= N; ++i )
       {
-         if( up_roads[ p*(N+1) + i ] )
+         if( is_up_road( p, i ) )
          {
-            if( !checked[i] )
-               to_process.push_back(i);
+            if( !checked[ i ] )
+            {
+               checked[ i ] = true;
+               to_process.push( i );
+            }
             up[i] = std::min( up[i], up[p] );
             if( down[p] != std::numeric_limits< uint32_t >::max() )
                up[i] = std::min( up[i], down[p]+1 );
          }
 
-         if( down_roads[ p*(N+1) + i ] )
+         if( is_up_road( i, p ) )
          {
-            if( !checked[i] )
-               to_process.push_back(i);
+            if( !checked[ i ] )
+            {
+               checked[ i ] = true;
+               to_process.push( i );
+            }
             down[i] = std::min( down[i], down[p] );
             if( up[p] != std::numeric_limits< uint32_t >::max() )
                down[i] = std::min( down[i], up[p]+1 );
