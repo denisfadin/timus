e95b31c2e9d7167c0076d75ed29fc3150aa18375
Denis Fadin <denis.fadin@gmail.com>
2021-11-11 20:27:43 +0300

    timus 1316

--- 1316/main.cpp
+++ 1316/main.cpp
@@ -1,13 +1,52 @@
 #include <cstdint>
 #include <cstdio>
 #include <cinttypes>
-#include <set>
+#include <vector>
+#include <math.h>
+
+inline uint32_t price_to_int( float x )
+{
+   return static_cast< uint32_t >( floorf( x * 100.0 + 0.5 ) );
+}
+
+void update( std::vector< uint32_t >& tree,
+             uint32_t v,
+             uint32_t tl, uint32_t tr,
+             uint32_t pos, int diff )
+{
+   if( tl == tr )
+		tree[ v ] += diff;
+	else
+   {
+		uint32_t tm = ( tl + tr ) / 2;
+		if( pos <= tm )
+			update( tree, v*2, tl, tm, pos, diff );
+		else
+			update( tree, v*2+1, tm+1, tr, pos, diff );
+      tree[ v ] = tree[ v*2 ] + tree[ v*2+1 ];
+	}
+}
+
+uint64_t sum( std::vector< uint32_t >& tree,
+              uint32_t v,
+              uint32_t tl, uint32_t tr,
+              uint32_t l, uint32_t r )
+{
+   if( l > r )
+		return 0;
+	if( l == tl && r == tr )
+		return tree[ v ];
+	uint32_t tm = (tl + tr) / 2;
+	return sum( tree, v*2, tl, tm, l, std::min( r, tm ) )
+		    + sum( tree, v*2+1, tm+1, tr, std::max( l, tm+1 ), r);
+}
 
 int main()
 {
-   uint64_t count = 0;
+   uint32_t const MAX = 1000000;
+   std::vector< uint32_t > segment_tree( 4*MAX, 0 );
 
-   std::multiset< float > data;
+   uint64_t count = 0;
 
    char buf[ 5 ];
    while( true )
@@ -18,14 +57,14 @@ int main()
          float x;
          std::scanf( "%f", &x );
 
-         data.insert( x );
+         update( segment_tree, 1, 1, MAX, price_to_int( x ), 1 );
       }
       else if( buf[ 0 ] == 'D' ) // DEL X
       {
          float x;
          std::scanf( "%f", &x );
 
-         data.erase( data.find( x ) );
+         update( segment_tree, 1, 1, MAX, price_to_int( x ), -1 );
       }
       else if( buf[ 0 ] == 'S' ) // SALE X K
       {
@@ -33,8 +72,8 @@ int main()
          uint32_t k;
          std::scanf( "%f %" SCNu32, &x, &k );
 
-         auto it = data.lower_bound( x );
-         count += std::min( k, static_cast< uint32_t >( std::distance( it, data.end() ) ) );
+         auto s = sum( segment_tree, 1, 1, MAX, price_to_int( x ), MAX );
+         count += std::min( static_cast< uint64_t >( k ), s );
       }
       else // QUIT
          break;
