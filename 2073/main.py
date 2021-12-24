cf272c519ad9679652b6c6b3b7b0af49b2c9ee36
Denis Fadin <denis.fadin@gmail.com>
2021-12-24 12:45:40 +0300

    timus 2073

--- 2073/main.py
+++ 2073/main.py
@@ -1,13 +1,18 @@
-def main():
+def print_result(contests):
     print('+------------------------------+--------+-------------+')
     print('|Contest name                  |Date    |ABCDEFGHIJKLM|')
     print('+------------------------------+--------+-------------+')
+    for contest in contests:
+        print('|{0: <30}|{1: <8}|{2: <13}|'.format(contest['name'], contest['date'], contest['tasks']))
+        print('+------------------------------+--------+-------------+')
+
+def read_contests():
+    contests = []
 
     N = int(input())
     for contest_id in range(N):
         name = input()
         date = input()
-
         p, s = map(int, input().split())
         tasks = ['.']*p
         for i in range(s):
@@ -19,9 +24,12 @@ def main():
                 tasks[task_id] = 'o'
             else:
                 tasks[task_id] = 'x'
+        contests.append({'name': name, 'date': date, 'tasks': ''.join(tasks)})
 
-        print('|{0: <30}|{1: <8}|{2: <13}|'.format(name, date, ''.join(tasks)))
-        print('+------------------------------+--------+-------------+')
+    return contests
+
+def main():
+    print_result(read_contests())
 
 if __name__ == '__main__':
     main()
